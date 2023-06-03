5333eb4a02b6c1e7afac9c88c4ee46ed0e59bcda
racine fellipe <racine.3301@gmail.com>
2023-06-03 08:09:21 -0300

    aplicando a função useState para armazernar o link do usuario e a url do qrcode criando a função para atualizar e gerar o qrcode em tempo de execução criação do link para baixar a img do qrcode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+
 
 function App() {
 
-  
+  const [link,setLink] = useState('');
+  const [qrcodeLInk, setQrcodeLink] = useState('');
+
+  function handleQrcode(e) {
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url,{
+      width: 600,
+      margin: 3,
+    }, function(err, url){
+      setQrcodeLink(url);
+    })
+  }
   return (
 
     <div className="container">
 
-      <h1>Projeto React</h1>
+      <QRCode
+        className='qrcode'
+        value = {link}
+      />
+
+      <input 
+        className='input'
+        placeholder="Digite seu Link..."
+        value = {link}
+        onChange={(e) => handleQrcode(e)}
+
+      />
+      <a href={qrcodeLInk} download={`qrcode.png`}>Baixar QrCode</a>
       
 
     </div>
